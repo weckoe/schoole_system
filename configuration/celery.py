f646277313a04d17655552c8e504153cdecac891
Nikita Chernolevskyi <nikita.chernolevsky@coxautoinc.com>
2022-03-20 19:54:34 +0100

    .

--- configuration/celery.py
+++ configuration/celery.py
@@ -1,17 +1,10 @@
-from __future__ import absolute_import
-
 import os
+
+from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuration.settings')
 
-import django
-from celery import Celery
-
-from .settings import INSTALLED_APPS
-
-django.setup()
-
-app = Celery('configuration')
+app = Celery('school')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
